7b97aebcf0a20914e4ecefaaf4c30c241ebe9701
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2020-06-06 15:00:10 +0530

    created Constructor

--- src/org/nbk/abstraction/device/Phone.java
+++ src/org/nbk/abstraction/device/Phone.java
@@ -4,12 +4,15 @@ import org.nbk.abstraction.category.Electronics;
 
 public abstract class Phone implements Electronics {
 
+    public Phone() {
+    }
+
     @Override
     public void display() {
-     System.out.println("DISPLAY");
-        
+	System.out.println("DISPLAY");
+
     }
-    
+
     public abstract void insertSIM();
 
 }
